04e4c2d92fc1e27cd132753a1b420787df0bd72b
G284602022 <228460@st.takushoku-u.ac.jp>
2023-05-11 17:30:28 +0900

    17:30　課題未完成

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -21,4 +21,21 @@ function hantei() {
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
+  for(kaisu=1;kaisu<5;kaisu++){
+
+    console.log(kaisu+"回目の予想:"+yoso);
+    if(kaisu>=4){
+        console.log("答えは"+kotae+"でした．すでにゲームは終わっています");
+    }else if(kotae===yoso){
+        console.log("正解です．おめでとう!");
+    }else if(kaisu===3){
+        console.log("まちがい．残念でした答えは"+kotae+"です．");   
+    }else if(kotae>=yoso){
+        console.log("まちがい．答えはもっと大きいですよ");
+    }else if(kotae<=yoso){
+        console.log("まちがい．答えはもっと小さいですよ");
+    }
+  }
+  
+
 }
