90c865879052624f936336e1fb161996bafff166
G284602022 <228460@st.takushoku-u.ac.jp>
2023-05-25 15:46:35 +0900

    5/25 15:46 一旦保存

--- kazuate.js
+++ kazuate.js
@@ -8,16 +8,15 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+
+let bt = document.querySelector('#print');
+bt.addEventListener('click', hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
-  
+  let intext = document.querySelector('input[name="seisu"]');
+  let yoso = parseInt(intext.value);  
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
@@ -38,7 +37,7 @@ function hantei() {
     let z = document.querySelector('span#answer');
     z.textContent=yoso;
     let y = document.querySelector('span#kaisu');
-    y.textContent=kaisu;
+    y.textContent=kaisu+"回目の予想: ";
     let x = document.querySelector('p#result');
     x.textContent=kekka;
 }
